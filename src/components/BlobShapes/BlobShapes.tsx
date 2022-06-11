684afb886ac79cae9d7f7f04f421d1e84a6965fa
Trevor Pirie <trevorpirie@gmail.com>
2022-06-11 16:35:50 +1200

    FancyBorders

--- src/components/BlobShapes/BlobShapes.tsx
+++ src/components/BlobShapes/BlobShapes.tsx
@@ -1,13 +1,14 @@
-import React, { FC } from 'react';
-import '../../styles/index.css';
-import './BlobShapes.css';
+import React, { FC } from 'react'
+import '../../styles/index.css'
+import './BlobShapes.css'
 
+// https://9elements.github.io/fancy-border-radius/
 const BlobShapes: FC = () => {
-  return (
-    <div className='container-center'>
-      <div className='blobby'>HAmster</div>
-    </div>
-  );
-};
+	return (
+		<div className='container-center'>
+			<div className='blobby'></div>
+		</div>
+	)
+}
 
-export { BlobShapes };
+export { BlobShapes }
