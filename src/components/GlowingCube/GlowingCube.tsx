c3954fae1c628298d81deca60aa43ec9e7171c1e
Trevor Pirie <trevorpirie@gmail.com>
2022-06-21 06:57:44 +1200

    create glowing cube

--- src/components/GlowingCube/GlowingCube.tsx
+++ src/components/GlowingCube/GlowingCube.tsx
@@ -2,20 +2,23 @@ import React, { FC } from 'react'
 import './GlowingCube.css'
 
 const GlowingCube: FC = () => {
+	const style0 = { '--i': 0 } as React.CSSProperties
+	const style1 = { '--i': 1 } as React.CSSProperties
+	const style2 = { '--i': 2 } as React.CSSProperties
+	const style3 = { '--i': 3 } as React.CSSProperties
+
 	return (
-		<div className='container'>
-			<div className='squirrel'>Squirrel</div>
+		<div className='glow-box-container'>
+			<div className='cube'>
+				<div className='top'></div>
+				<div>
+					<span style={style0}></span>
+					<span style={style1}></span>
+					<span style={style2}></span>
+					<span style={style3}></span>
+				</div>
+			</div>
 		</div>
-		// <div className='glow-box-container'>
-		// 	<div className='cube'>
-		// 		<div>
-		// 			<span className='style0'></span>
-		// 			<span className='style1'></span>
-		// 			<span className='style2'></span>
-		// 			<span className='style3'></span>
-		// 		</div>
-		// 	</div>
-		// </div>
 	)
 }
 
