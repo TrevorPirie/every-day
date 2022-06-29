c9cc01b41a5ffd408d406d595e531f5230f182c2
TrevorPirie <drizztpirie@hotmail.com>
2022-06-29 12:40:30 +1200

    Using ref to track

--- src/components/GlassMorph/GlassMorph.tsx
+++ src/components/GlassMorph/GlassMorph.tsx
@@ -1,13 +1,40 @@
-import React, { FC, useEffect, useRef } from 'react';
+import React, { FC, MutableRefObject, useEffect, useRef } from 'react';
 import './GlassMorph.css';
 import VanillaTilt from 'vanilla-tilt';
 
 const GlassMorph: FC = (props: any) => {
-  const tilt = useRef([]);
+  const tiltRef = useRef<HTMLDivElement[]>([]);
+
+  const employees = [
+    {
+      name: 'John Smith',
+      role: 'Lead Scientist',
+    },
+    {
+      name: 'Alex Morgan',
+      role: 'Data Analyst',
+    },
+    {
+      name: 'Rowan Anderson',
+      role: 'Botanist',
+    },
+    {
+      name: 'Blake Robinson',
+      role: 'Software Engineer',
+    },
+    {
+      name: 'Jeff Stone',
+      role: 'Scientist',
+    },
+    {
+      name: 'Peter Rowlands',
+      role: 'Geologist',
+    },
+  ];
 
   useEffect(() => {
-    if (tilt?.current)
-      VanillaTilt.init(tilt.current, {
+    if (tiltRef?.current)
+      VanillaTilt.init(tiltRef.current, {
         max: 25,
         speed: 400,
         glare: true,
@@ -18,21 +45,46 @@ const GlassMorph: FC = (props: any) => {
   return (
     <div className='glass-morph-body'>
       <div className='glass-morph-container'>
-        <div ref={(el) => (tilt.current[0] = el)} className='card'>
+        {employees.map((element, index) => (
+          <div
+            key={index}
+            ref={(el: HTMLDivElement) => {
+              if (el) tiltRef.current[index] = el as HTMLDivElement;
+            }}
+            className='card'
+          >
+            <h2>{index <= 8 ? `0${index + 1}` : index}</h2>
+            <h3>{element.name}</h3>
+            <p>Role: {element.role}</p>
+          </div>
+        ))}
+        {/* <div
+          key={0}
+          ref={(el: HTMLDivElement) => {
+            if (el) tiltRef.current[0] = el as HTMLDivElement;
+          }}
+          className='card'
+        >
           <div className='content'>
             <h2>01</h2>
             <h3>John Smith</h3>
             <p>Role: Lead Scientist</p>
           </div>
-        </div>
-        <div ref={tilt} className='card'>
+        </div> */}
+        {/* <div
+          key={1}
+          ref={(el: HTMLDivElement) => {
+            if (el) tiltRef.current[1] = el as HTMLDivElement;
+          }}
+          className='card'
+        >
           <div className='content'>
             <h2>02</h2>
             <h3>Alex Morgan</h3>
             <p>Role: Botanist</p>
           </div>
-        </div>
-        <div ref={tilt} className='card'>
+        </div> */}
+        {/* <div ref={tilt} className='card'>
           <div className='content'>
             <h2>03</h2>
             <h3>Andrew Schofield</h3>
@@ -45,7 +97,7 @@ const GlassMorph: FC = (props: any) => {
             <h3>Andrew Schofield</h3>
             <p>Role: Physicist</p>
           </div>
-        </div>
+        </div>*/}
       </div>
     </div>
   );
